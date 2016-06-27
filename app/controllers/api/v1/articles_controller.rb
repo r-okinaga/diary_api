41b82caab5a8021b75451f5379a020b84e828048
r-okinaga <Hrnn0PDk1>
2016-06-27 19:28:49 +0900

    json

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,11 @@
+module Api
+    module V1
+        class ArticlesController < ApplicationController
+
+            def index
+                @articles = Article.all
+                render json: @articles
+            end
+        end
+    end
+end
